ffa2665decbf47eb2a1ce642944144061454940c
Giuseppe Lettieri <g.lettieri@iet.unipi.it>
2012-08-30 16:12:25 +0200

    list files that do not go into the distribution
    The Makefile of Open vSwitch checks that all files in the
    git repository are also distributed and stops with an error
    otherwise. But we have some files in git that are not meant
    to be distributed (i.e., installed on a sliver). These files can now
    be listed in .non-distfiles and the new Makefile will ignore them.

--- planetlab/automake.mk
+++ planetlab/automake.mk
@@ -3,15 +3,16 @@ sbin_PROGRAMS += \
 	planetlab/vsysc/vsysc
 
 # this Makefile is not intended to go on the sliver image - esp. not in /usr/sbin
-#	planetlab/scripts/Makefile \
+#	planetlab/scripts/Makefile
+# same goes for showgraph
+#	planetlab/scripts/showgraph
 dist_sbin_SCRIPTS += \
 	planetlab/scripts/start_ovsdb-server \
 	planetlab/scripts/start_vswitchd \
 	planetlab/scripts/create_bridge \
 	planetlab/scripts/create_port \
 	planetlab/scripts/del_bridge \
-	planetlab/scripts/del_port \
-	planetlab/scripts/showgraph
+	planetlab/scripts/del_port
 
 planetlab_pltap_ovs_pltap_ovs_SOURCES = \
 	planetlab/pltap-ovs/pltap-ovs.c \
