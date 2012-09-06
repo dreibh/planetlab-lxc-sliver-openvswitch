48082a127709cef14cd75a4d6deedc59ae2d13ba
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2012-09-06 08:25:32 +0200

    fix dependencies after scripts cleanup

--- planetlab/automake.mk
+++ planetlab/automake.mk
@@ -1,23 +1,14 @@
-sbin_PROGRAMS += \
-	planetlab/pltap-ovs/pltap-ovs \
-	planetlab/vsysc/vsysc
+sbin_PROGRAMS += planetlab/pltap-ovs/pltap-ovs 
+sbin_PROGRAMS += planetlab/vsysc/vsysc
 
 # this Makefile is not intended to go on the sliver image - esp. not in /usr/sbin
 #	planetlab/scripts/Makefile
 # same goes for showgraph
 #	planetlab/scripts/showgraph
-dist_sbin_SCRIPTS += \
-	planetlab/scripts/start_ovsdb-server \
-	planetlab/scripts/start_vswitchd \
-	planetlab/scripts/create_bridge \
-	planetlab/scripts/create_port \
-	planetlab/scripts/del_bridge \
-	planetlab/scripts/del_port
+dist_sbin_SCRIPTS += planetlab/scripts/sliver-ovs 
 
-planetlab_pltap_ovs_pltap_ovs_SOURCES = \
-	planetlab/pltap-ovs/pltap-ovs.c \
-	planetlab/pltap-ovs/tunalloc.c \
-	planetlab/pltap-ovs/tunalloc.h
+planetlab_pltap_ovs_pltap_ovs_SOURCES += planetlab/pltap-ovs/pltap-ovs.c
+planetlab_pltap_ovs_pltap_ovs_SOURCES += planetlab/pltap-ovs/tunalloc.c
+planetlab_pltap_ovs_pltap_ovs_SOURCES += planetlab/pltap-ovs/tunalloc.h
 
-planetlab_vsysc_vsysc_SOURCES = \
-	planetlab/vsysc/vsysc.c
+planetlab_vsysc_vsysc_SOURCES += planetlab/vsysc/vsysc.c
